ed31629e0603875aa41bbc56f20b2d99299a1ea6
nina75 <kozasbrada28>
2016-11-17 23:24:34 +0200

    03.Decimal to hexadecimal

---
+++ NumeraSystems/BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,24 @@
+﻿//Write a program that converts a binary number N to its decimal representation.
+using System;
+class BinaryToDecimal
+{
+    static void Main()
+    {
+        string binaryNumber = Console.ReadLine();
+        Console.WriteLine(ConvertBinaryToDecimal(binaryNumber));
+    }
+
+    static long ConvertBinaryToDecimal(string binaryNum) // 110011 - 1*2^0 + 1*2^1 + 0*2^2 +...
+    {
+        long number = 0;
+        int multiplier = 1;
+        for (int i = binaryNum.Length - 1; i >= 0 ; i--)
+        {
+            int coeficiente = binaryNum[i] == '0' ? 0 : 1;
+            number += coeficiente * multiplier;
+            multiplier <<= 1;
+        }
+
+        return number;
+    }
+}
---
+++ NumeraSystems/DecimalToHexadecimal/DecimalToHexadecimal.cs
@@ -0,0 +1,29 @@
+﻿//Write a program that converts a decimal number N to its hexadecimal representation.
+using System;
+using System.Text;
+
+class DecimalToHexadecimal
+{
+    static void Main()
+    {
+        long num = long.Parse(Console.ReadLine());
+       // char[] symbols = { '0', '1' };
+        //Console.WriteLine(symbols[0]); 
+        Console.WriteLine(ConvertDecimalToHexadecimal(num));
+
+    }
+
+    static string ConvertDecimalToHexadecimal(long number)
+    {
+        char[] symbols = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        var hexNum = new StringBuilder(); 
+        while (number > 0)
+        {
+            //char remainder = symbols[number % 16];
+            hexNum.Insert(0, symbols[number % 16]);
+            number /= 16;
+        }
+
+        return hexNum.ToString();
+    }
+}
