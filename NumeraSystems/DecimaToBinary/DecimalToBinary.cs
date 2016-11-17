736504fdcbaa5c9f006ed5995b09347e634cf343
nina75 <kozasbrada28>
2016-11-17 22:32:25 +0200

    01.Decimal to binary

---
+++ NumeraSystems/DecimaToBinary/DecimalToBinary.cs
@@ -0,0 +1,22 @@
+﻿//Write a program that converts a decimal number N to its binary representation.
+using System;
+using System.Collections.Generic;
+
+class DecimalToBinary
+{
+    static void Main()
+    {
+        long number = long.Parse(Console.ReadLine()); //19
+        var bits = new List<long>();  
+        while (number > 0)
+        {
+            bits.Add(number % 2);
+            number /= 2;
+        }
+
+        long[] bitsArray = bits.ToArray();
+        Array.Reverse(bitsArray);
+        Console.WriteLine(String.Join("", bitsArray));
+        
+    }
+}
